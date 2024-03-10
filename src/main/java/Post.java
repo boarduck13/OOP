5df5697883ff7c13120c063a8220c19b666bd35f
Andrey <justupor@gmail.com>
2024-03-10 17:19:17 +0300

    add a birthday field with a description of the field in the FormDate class

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,10 +1,7 @@
 public class Post {
+    FormDate birthday = new FormDate(); //обозначили поле birthday отдельным классом
     public String name; // поле для имени
     // добавьте другие поля
     public String passport;
     public String patronymic;
     public String phone;
-    public String surname;
-    public boolean subscription;
-
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,16 @@ public class Main {
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
         post.subscription = true;
+        post.birthday.day = 13;   //заполнили поле day в поле birthday
+        post.birthday.month = 6;  //заполнили поле month в поле birthday
+        post.birthday.year = 1999; //заполнили поле year в поле birthday
+
+        post.name = "Иван";                 //заполнили поле name
+        post.passport = "4444 № 44444444";  //заполнили поле passport
+        post.patronymic = "Иванович";       //заполнили поле patronymic
+        post.phone = "+7 (999)-999-99-99";  //заполнили поле phone
+        post.surname = "Иванов";            //заполнили поле surname
+        post.subscription = true;           //заполнили поле subscription
 
     }
 }
